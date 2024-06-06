6c52003b0519b07ebdcefd3d38f96a4fe27f6cff
Weslei França <weslwifranca@gmail.com>
2024-06-06 20:13:17 -0300

    update sendMail

--- src/app/controlles/ConfirmEmail.js
+++ src/app/controlles/ConfirmEmail.js
@@ -6,8 +6,12 @@ import nodemailer from 'nodemailer'
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: process.env.EMAIL,
     pass: process.env.PASSWORD_EMAIL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
   },
 })
 
--- src/app/sendMail/SendEmailRequeriment/index.js
+++ src/app/sendMail/SendEmailRequeriment/index.js
@@ -5,8 +5,12 @@ import * as Yup from 'yup'
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: process.env.EMAIL,
     pass: process.env.PASSWORD_EMAIL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
   },
 })
 
